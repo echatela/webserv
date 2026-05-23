e9b0e7676294c543c756f9977d9f2004ec2a6e06
echatela <edchatelain9@gmail.com>
2026-05-23 12:11:48 +0200

    epoll is working approximatively

--- Server.hpp
+++ Server.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <netinet/in.h>
+#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+
+#define MAX_EVENTS	128
 
 class Server
 {
@@ -13,13 +16,20 @@ private:
 	int	_protocol;
 	int	_backlog;
 	u_long	_interface;
-
-	int	_socket;
 	struct sockaddr_in	_address;
 
+	int	_socket;
+
+	int	_eventsFlags;
+	struct epoll_event	_ev;
+	struct epoll_event	_events[MAX_EVENTS];
+
+	int	_epfd;
+
 public:
-	Server(int domain = AF_INET, int port = 80, int service = SOCK_STREAM,
-	int protocol = 0, int backlog = 10, u_long interface = INADDR_ANY);
+	Server(int domain = AF_INET, int port = 8080, int service = SOCK_STREAM,
+	int protocol = 0, int backlog = 10, u_long interface = INADDR_ANY,
+	int eventsFlags = EPOLLIN | EPOLLET);
 	Server(const Server & src);
 	~Server();
 	Server &	operator=(const Server & rhs);
