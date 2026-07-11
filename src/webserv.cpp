afaac496c2505fc6a6654c9aab3bb378f16cc498
echatela <edchatelain9@gmail.com>
2026-07-11 07:48:32 +0200

    simpler setnonblock and setcloexec for subject requirement

--- src/webserv.cpp
+++ src/webserv.cpp
@@ -12,18 +12,12 @@
 
 int	webserv::fd::SetNonBlock(int fd)
 {
-	int flags = fcntl(fd, F_GETFL, 0);
-	if (flags == -1)
-		return -1;
-	return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+	return fcntl(fd, F_SETFL, O_NONBLOCK);
 }
 
 int	webserv::fd::SetCloExec(int fd)
 {
-	int flags = fcntl(fd, F_GETFD, 0);
-	if (flags == -1)
-		return -1;
-	return fcntl(fd, F_SETFD, flags | FD_CLOEXEC);
+	return fcntl(fd, F_SETFD, FD_CLOEXEC);
 }
 
 std::string	webserv::utils::IntToStr(int value)
